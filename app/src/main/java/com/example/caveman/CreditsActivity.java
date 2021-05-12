bf828587f67c2b3a56520be656d9ad98b606f531
Akash Ramanand Rajak <aakashrajak02@gmail.com>
2021-05-13 01:22:16 +0530

    Added github code link in credit section

--- app/src/main/java/com/example/caveman/CreditsActivity.java
+++ app/src/main/java/com/example/caveman/CreditsActivity.java
@@ -6,8 +6,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 import android.widget.ViewFlipper;
 
@@ -51,6 +53,9 @@ public class CreditsActivity extends Activity {
 				}
 			}
 		});
+
+		TextView textView = (TextView)findViewById(R.id.textView);
+		textView.setMovementMethod(LinkMovementMethod.getInstance());
 
 	}
 
